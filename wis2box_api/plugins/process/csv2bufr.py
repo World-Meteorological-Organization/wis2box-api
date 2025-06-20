0e42b84b23f614a232cb66686a772bbd92dd7d18
Maaike <maaike.limper@gmail.com>
2025-06-20 16:31:18 +0200

    project against no geometry/coordinates in geometry

--- wis2box_api/plugins/process/csv2bufr.py
+++ wis2box_api/plugins/process/csv2bufr.py
@@ -212,7 +212,7 @@ class CSVPublishProcessor(BaseProcessor):
                     # compare geometry in _meta with station geometry
                     geo_station = stations.get_geometry(wsi)
                     geo_data = item['_meta'].get('geometry', None)
-                    if geo_station and geo_data:
+                    if geo_station and geo_data and 'coordinates' in geo_station and 'coordinates' in geo_data: # noqa
                         s_lon, s_lat = geo_station['coordinates'][0:2]
                         d_lon, d_lat = geo_data['coordinates'][0:2]
                         station_coord = (s_lat, s_lon)
